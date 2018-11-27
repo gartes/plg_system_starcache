6b59e598ef84f14b3252122615197341a09cb3df
gartes <sad.net79@gmail.com>
2018-11-27 22:03:07 +0200

    1

---
+++ helpers/scripts.php
@@ -0,0 +1,30 @@
+<?php
+	namespace starcache\helpers ;
+	/**
+	 * @package     helpers
+	 * @subpackage
+	 *
+	 * @copyright   A copyright
+	 * @license     A "Slug" license name e.g. GPL2
+	 */
+	
+	class scripts
+	{
+		
+		
+		
+		/**
+		 * scripts constructor.
+		 *
+		 * @param $subject
+		 * @param $config
+		 */
+		public function __construct( ){
+		
+		}
+		
+		
+		
+		
+		
+	} 
